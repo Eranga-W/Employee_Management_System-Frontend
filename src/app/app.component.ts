1b4eac3bd0cb8bf83be922054899ef041b29450c
Eranga-W <wadewickramasinghe@gmail.com>
2024-07-18 16:29:18 +0530

    view all employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpolyeeComponent } from './page/manage-empolyee/manage-empolyee.component';
+import { ViewAllEmployeeComponent } from './page/view-all-employee/view-all-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmpolyeeComponent],
+  imports: [RouterOutlet,ManageEmpolyeeComponent,ViewAllEmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
