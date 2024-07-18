f9ccb11fcc261ea45ffa11405280cbb5a5c26443
Eranga-W <wadewickramasinghe@gmail.com>
2024-07-18 17:52:03 +0530

    navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ManageEmpolyeeComponent } from './page/manage-empolyee/manage-empolyee.component';
+import { ViewAllEmployeeComponent } from './page/view-all-employee/view-all-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-employee",
+        component:ManageEmpolyeeComponent
+    },
+    {
+        path:"view-all-employee",
+        component:ViewAllEmployeeComponent
+    }
+];
